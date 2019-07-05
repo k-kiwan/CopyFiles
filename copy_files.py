88c343914c7b70e44476e6647e106d407440d540
k-kiwan <34815551+k-kiwan@users.noreply.github.com>
2019-07-05 19:10:18 +0200

    added looping through subfolders

--- copy_files.py
+++ copy_files.py
@@ -3,20 +3,25 @@ import os
 import re
 
 
-src_path = r'\\eutct.internal.towerswatson.com\DavWWWRoot\nonclients\GECATAnalysisBelgium\Documents\FromFolder'
-dest_path = r'\\eutct.internal.towerswatson.com\DavWWWRoot\nonclients\GECATAnalysisBelgium\Documents\ToFolder'
-source_files = os.listdir(src_path)
-source_folders = os.listdir(r'\\eutct.internal.towerswatson.com\DavWWWRoot\nonclients\GECATAnalysisBelgium\Documents')
+src_path = r'//eutct.internal.towerswatson.com/nonclients/EMEADDU/Documents/2019/Data_Collection_Worksheets_by_Company/Belgium'
+dest_path = r'//eutct.internal.towerswatson.com/nonclients/GECATAnalysisBelgium/Documents/2019/LTI Valuations/Test'
+
+source_folders_list = os.listdir(src_path)
 
 
-for element in source_files:
-    if re.match('WE',element):
-        #newf = folder.split('.')[0] 
-    #newf is name of new folder where you want to move 
-    #change Folder name as per yourrequirement
-        destination = dest_path
-        if not os.path.exists(destination):
-            os.makedirs(destination)
-        shutil.copy(src_path+'\\'+element,destination) #change move to copy if you want to copy insted of moving 
-        print('done moving')
-    else: print('no such file')
+for folder in source_folders_list:
+        source_folder = src_path + '/' + folder
+        files_list = os.listdir(source_folder)
+        for element in files_list:
+            if re.match('^.*QA.*$',element):
+            #newf = folder.split('.')[0] 
+            #newf is name of new folder where you want to move 
+            #change Folder name as per yourrequirement
+                destination = dest_path
+                if not os.path.exists(destination):
+                    os.makedirs(destination)
+                shutil.copy(src_path+'/'+folder+'/'+element,destination) #change move to copy if you want to copy insted of moving 
+                print('done copying')
+            else: print('no such file in ' + folder)
+
+stop = input("Press Enter to close")
